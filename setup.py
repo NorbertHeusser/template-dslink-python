37aeddf5ab82f9366c55dafe30ceeed5d5f64dc4
Logan Gorence <loganjohngorence@gmail.com>
2016-07-19 10:53:09 -0700

    Update to use the latest SDK.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="dslink-python-grovepi",
+    name="dslink-python-template",
     version="0.1.0",
-    description="DSLink for GrovePi",
-    url="http://github.com/IOT-DSA/dslink-python-grovepi",
+    description="DSLink Template",
+    url="http://github.com/IOT-DSA/template-dslink-python",
     author="Logan Gorence",
     author_email="l.gorence@dglogik.com",
     license="Apache 2.0",
     install_requires=[
-        "dslink >=0.5.8, <=0.6.0"
+        "dslink == 0.6.16"
     ]
 )
--- src/TemplateDSLink.py
+++ src/TemplateDSLink.py
@@ -4,21 +4,17 @@ from twisted.internet import reactor
 
 
 class TemplateDSLink(dslink.DSLink):
-    def __init__(self, config):
+    def start(self):
         self.random = random.Random()
         self.speed = 0.1
-        dslink.DSLink.__init__(self, config)
 
-    def start(self):
-        self.profile_manager.create_profile("set_speed")
-        self.profile_manager.register_callback("set_speed", self.set_speed)
+        self.responder.profile_manager.create_profile("set_speed")
+        self.responder.profile_manager.register_callback("set_speed", self.set_speed)
 
         reactor.callLater(0.1, self.poll)
 
-    def get_default_nodes(self):
-        root = self.get_root_node()
-
-        set_speed = dslink.Node("set_speed", root)
+    def get_default_nodes(self, super_root):
+        set_speed = super_root.create_child("set_speed")
         set_speed.set_parameters([
             {
                 "name": "Speed",
@@ -36,22 +32,18 @@ class TemplateDSLink(dslink.DSLink):
         set_speed.set_profile("set_speed")
         set_speed.set_invokable("config")
 
-        test_one = dslink.Node("test_one", root)
+        test_one = super_root.create_child("test_one")
         test_one.set_type("number")
         test_one.set_value(0)
 
-        test_two = dslink.Node("test_two", root)
+        test_two = super_root.create_child("test_two")
         test_two.set_type("number")
         test_two.set_value(0)
 
-        root.add_child(test_one)
-        root.add_child(test_two)
-        root.add_child(set_speed)
-
-        return root
+        return super_root
 
     def set_speed(self, parameters):
-        self.speed = int(parameters.params["Speed"])
+        self.speed = int(parameters[1]["Speed"])
 
         return [
             [
@@ -61,8 +53,8 @@ class TemplateDSLink(dslink.DSLink):
 
     def poll(self):
         # Poll data here and set the values
-        self.super_root.get("/test_one").set_value(self.random.random())
-        self.super_root.get("/test_two").set_value(self.random.random())
+        self.responder.super_root.get("/test_one").set_value(self.random.random())
+        self.responder.super_root.get("/test_two").set_value(self.random.random())
 
         reactor.callLater(self.speed, self.poll)
 
