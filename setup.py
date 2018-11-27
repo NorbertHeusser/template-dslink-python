49e5fc73bf131b40e8ea00a80342b7a4892c1cee
Norbert Heusser <nheusser@cisco.com>
2018-11-27 07:16:06 +0100

    Changed sdk url to cloned repository

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ setup(
     name="dslink-python-template",
     version="0.2.1",
     description="Python DSLink Template",
-    url="http://github.com/IOT-DSA/template-dslink-python",
+    url="http://github.com/NorbertHeusser/template-dslink-python.git",
     author="Logan Gorence",
     author_email="Logan.Gorence@AcuityBrands.com",
     license="Apache 2.0",
     install_requires=[
-        "dslink == 0.7.2"
+        "dslink == 0.7.3.1"
     ],
     dependency_links=[
-        "https://github.com/IOT-DSA/sdk-dslink-python/archive/v0.7.1.tar.gz#egg=dslink-0.7.2"
+        "https://github.com/NorbertHeusser/sdk-dslink-python/archive/v0.7.3.1.tar.gz#egg=dslink-0.7.3.1"
     ]
 )
