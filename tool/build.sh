f319e4e4bcfb6dcc0437c72afaf81ba15a352019
Norbert Heusser <nheusser@cisco.com>
2018-11-27 20:52:34 +0100

    Added name config entry to dslink.json file and improved build.sh to get name and version for the package from the dslink.json

--- tool/build.sh
+++ tool/build.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-zip ../../files/dslink-python-template.zip -r src dslink.json setup.py
+
+NAME=`grep name dslink.json | head -1 | awk '{ print $2 }' | tr -d '","'`
+if [ -z "${NAME}" ]; then
+    echo "Cannot determinate name from dslink.json" 1>&2
+    exit 1
+fi
+
+VERSION=`grep version dslink.json | awk '{ print $2 }' | tr -d '","'`
+if [ -z "${VERSION}" ]; then
+    echo "Cannot determinate version from dslink.json" 1>&2
+    exit 1
+fi
+
+
+zip ./${NAME}_${VERSION}.zip -r src dslink.json setup.py
+
+echo "Created " `pwd`/"${NAME}_${VERSION}.zip"
